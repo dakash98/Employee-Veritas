4a3e9f08390bdad0955ef7cc924ab6b3e68653c8
Akash Deep <akash.deep@sait.ac.in>
2022-02-03 23:05:51 +0530

    create basic template for signUp page

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,14 +1,51 @@
 <template>
-  <div class="sign-up">
-    <h3>Add Sign Up Page</h3>
-    <br /><br />
-    <h4>Already Sign Up ? <router-link to="/">Login</router-link></h4>
+  <div class="outer">
+    <div class="inner">
+      <BaseInput v-model="user.email" label="Username" type="text" />
+      <br /><br />
+
+      <BaseInput v-model="user.password" label="Password" type="password" />
+      <br /><br />
+
+      <button data-testid="submit" @click="registerUser">SignUp</button>
+      <br /><br />
+      <h4>Already Registered ? <router-link to="/">Login</router-link></h4>
+    </div>
   </div>
 </template>
 
+<script>
+// import server from "@/services/jsonServer.js";
+export default {
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    registerUser() {
+      console.log("registering");
+    },
+    clearFormInputs() {
+      this.user["email"] = "";
+      this.user["password"] = "";
+    },
+  },
+};
+</script>
+
 <style>
-.sign-up {
+.outer {
+  width: 100%;
+}
+.inner {
   display: table;
   margin: 0 auto;
+}
+.set-color {
+  color: white;
 }
 </style>
